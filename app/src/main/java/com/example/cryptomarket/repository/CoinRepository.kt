8f9841161c0ef35448ba0f70ed8b1589da8becda
petro.ubohyi <petro.ubohyi@globallogic.com>
2022-09-02 14:06:01 +0300

    create request in coin repository and prepare viewmodel

--- app/src/main/java/com/example/cryptomarket/repository/CoinRepository.kt
+++ app/src/main/java/com/example/cryptomarket/repository/CoinRepository.kt
@@ -1,6 +1,9 @@
 package com.example.cryptomarket.repository
 
+import android.util.Log
 import com.example.cryptomarket.repository.api.CoinApi
+import com.example.cryptomarket.repository.api.model.toCoinItem
+import com.example.cryptomarket.ui.model.CoinItem
 import javax.inject.Inject
 
 private const val API_KEY = "coinranking3ac517ca7ab117b9224a8a6fe33335355b97ea7191701afc"
@@ -8,8 +11,16 @@ private const val API_KEY = "coinranking3ac517ca7ab117b9224a8a6fe33335355b97ea71
 class CoinRepository @Inject constructor(
     private val coinApi: CoinApi
 ) {
-    suspend fun getCoins(): String {
-        val response = coinApi.getCoins(API_KEY)
-        return response.body()?.data?.coins?.get(0)?.name ?: "response null"
+    suspend fun getCoins(): List<CoinItem> {
+        return try {
+            val response = coinApi.getCoins(API_KEY)
+            val result = response.body()?.data?.coins?.map { coin ->
+                coin.toCoinItem()
+            }
+            result ?: emptyList()
+        } catch (ex: Exception) {
+            Log.d("GET COINS", ex.toString())
+            emptyList()
+        }
     }
 }
--- app/src/main/java/com/example/cryptomarket/ui/coins_list/CoinsViewModel.kt
+++ app/src/main/java/com/example/cryptomarket/ui/coins_list/CoinsViewModel.kt
@@ -1,14 +1,33 @@
 package com.example.cryptomarket.ui.coins_list
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cryptomarket.repository.CoinRepository
+import com.example.cryptomarket.ui.model.CoinItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class CoinsViewState(
+    val coins: List<CoinItem>,
+    val isLoading: Boolean = true,
+)
 
 @HiltViewModel
 class CoinsViewModel @Inject constructor(
     private val coinRepository: CoinRepository
 ) : ViewModel() {
 
-    suspend fun getCoins(): String = coinRepository.getCoins()
+    private val _coinsViewState = MutableLiveData<CoinsViewState>()
+    val coinsViewState: LiveData<CoinsViewState>
+        get() = _coinsViewState
+
+    init {
+        viewModelScope.launch {
+            val coins = coinRepository.getCoins()
+            _coinsViewState.value = CoinsViewState(coins = coins, isLoading = false)
+        }
+    }
 }
